71e576909afdfec1862f4b681a708ac127e4025c
Everett Kleven <ekleven@vangelis.tech>
2025-11-21 13:42:31 -0800

    add mm_structured_outputs notebook draft 1

--- usage_patterns/prompt/prompt_structured_outputs.py
+++ usage_patterns/prompt/prompt_structured_outputs.py
@@ -3,12 +3,19 @@
 # requires-python = ">=3.10, <3.13"
 # dependencies = ["daft","openai","pydantic","python-dotenv", "numpy"]
 # ///
+import os
 import daft
 from daft.functions import unnest, prompt
 from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
+
+daft.set_provider(
+    "openai", 
+    api_key=os.environ.get("OPENROUTER_API_KEY"), 
+    base_url="https://openrouter.ai/api/v1"
+)
 
 class Anime(BaseModel):
     show: str = Field(description="The name of the anime show")
@@ -32,9 +39,9 @@ df = df.with_column(
         daft.col("quote"),
         system_message="You are an anime expert. Classify the anime based on the text and return the name, character, and quote.",
         return_format=Anime,
-        provider="openai",
-        model="gpt-5-nano",
+        model="nvidia/nemotron-nano-9b-v2:free",
+        use_chat_completions=True,
     ),
 ).select("quote", unnest(daft.col("response")))
 
-df.show(format="fancy", max_width=120)
+df.show(format="fancy", max_width=80)
