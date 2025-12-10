40c797443456a722a918bfc639e3868d8ad7086c
Everett Kleven <ekleven@vangelis.tech>
2025-12-10 13:07:33 -0800

    update env vars

--- use_cases/social_recommendation/index_reddit_irl_images.py
+++ use_cases/social_recommendation/index_reddit_irl_images.py
@@ -17,9 +17,9 @@ class ImageWriter:
     def __init__(self):
         self.session = aioboto3.Session(
             region_name="us-west-2",
-            aws_access_key_id=os.getenv("S3_ACCESS_KEY_ID"),
-            aws_secret_access_key=os.getenv("S3_SECRET_ACCESS_KEY"),
-            aws_session_token=os.getenv("S3_SESSION_TOKEN")
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+            aws_session_token=os.getenv("AWS_SESSION_TOKEN")
         )
 
     async def write_images(self, image_bytes: bytes, path: str) -> str:
