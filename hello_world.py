ec8fc971374ddfb1579c004fe5b2c0207179093a
Jay Chia <17691182+jaychia@users.noreply.github.com>
2025-12-17 15:14:37 +0800

    use openai embeddings

--- hello_world.py
+++ hello_world.py
@@ -27,6 +27,6 @@ def example():
     )
     data = data.with_column(
         "embedding",
-        embed_text(text=data["content"], model="Qwen/Qwen3-Embedding-0.6B")
+        embed_text(text=data["content"], model="openai/text-embedding-3-small")
     )
     return data
